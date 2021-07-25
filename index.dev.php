c111e3bd32c409141230fcd58b208e71b62c398b
Alex Vagulis <vagulisal500@gmail.com>
2021-07-25 15:54:19 +0300

    fix

--- index.dev.php
+++ index.dev.php
@@ -23,13 +23,13 @@
         <div class="row bg-header-flex">
             <div class="col-md-6 col-sm-6 col-xs-6 text-left">
                 <h4 class="text-left ask_title fonts_def_1 font_size_25">
-                    <img width="150" src="image/logo-holodok.png" alt="">
+                    <img width="170" src="image/logo-holodok.png" alt="">
                 </h4>
             </div>
             <div class="col-md-6 col-sm-6 col-xs-6 text-right">
                 <span class="phone">
                 <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="phone-alt" class="svg-inline--fa fa-phone-alt fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M497.39 361.8l-112-48a24 24 0 0 0-28 6.9l-49.6 60.6A370.66 370.66 0 0 1 130.6 204.11l60.6-49.6a23.94 23.94 0 0 0 6.9-28l-48-112A24.16 24.16 0 0 0 122.6.61l-104 24A24 24 0 0 0 0 48c0 256.5 207.9 464 464 464a24 24 0 0 0 23.4-18.6l24-104a24.29 24.29 0 0 0-14.01-27.6z"></path></svg>
-                    <a href="tel:+375255259000">+375 25 525 9000</a>
+                    <a href="tel:+375255259000">+375255259000</a>
                 </span>
             </div>
         </div>
@@ -37,9 +37,9 @@
 </header>
 <section>
     <div class="fire-bg">
-        <h2 class="wow rubberBand2"><span>МЫ ПОКОРЯЕМ</span> ЖАРУ!</h2>
+        <h2 class="wow rubberBand2"><span>ДАЕМ ОТПОР</span> ЖАРЕ!</h2>
         <img class="fire-img" src="image/fire.jpg" alt="">
-        <h3 class="fire-bg-sub-title"><span>Победа</span> будет за нами</h3>
+        <h3 class="fire-bg-sub-title">Победа будет за нами</h3>
     </div>
 </section>
 <!--Заправка кондиционера-->
