f92fe29661eb047b939c60b0d905665d98082857
Alex Vagulis <vagulisal500@gmail.com>
2021-07-26 21:38:41 +0300

    fix

---
+++ index.php
@@ -0,0 +1,338 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Заправка кондиционера в Минске</title>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <meta name="description" content>
+    <meta name="keywords" content>
+    <link rel="icon" href="image/favicon.ico" type="image/x-icon">
+    <!--    <link rel="shortcut icon" href="images/favicon0.ico" type="image/x-icon">-->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="/libs/bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+    <script src="https://api-maps.yandex.ru/2.1/?apikey=c31d9a3b-860a-41f0-b8fb-708a953e14c3&lang=ru_RU"
+            type="text/javascript"></script>
+    <link rel="stylesheet" href="libs/wow_js/index.min.css">
+
+</head>
+<body>
+
+<!-- Header -->
+<header class="bg-header">
+    <div class="container">
+        <div class="row bg-header-flex">
+            <div class="col-md-6 col-sm-6 col-xs-6 text-left">
+                <h4 class="text-left ask_title fonts_def_1 font_size_25">
+                    <img width="200" src="image/logo-holodno-v-2.png" alt="">
+                </h4>
+            </div>
+            <div class="col-md-6 col-sm-6 col-xs-6 text-right">
+                <span class="phone">
+                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="phone-alt"
+                     class="svg-inline--fa fa-phone-alt fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg"
+                     viewBox="0 0 512 512"><path fill="currentColor"
+                                                 d="M497.39 361.8l-112-48a24 24 0 0 0-28 6.9l-49.6 60.6A370.66 370.66 0 0 1 130.6 204.11l60.6-49.6a23.94 23.94 0 0 0 6.9-28l-48-112A24.16 24.16 0 0 0 122.6.61l-104 24A24 24 0 0 0 0 48c0 256.5 207.9 464 464 464a24 24 0 0 0 23.4-18.6l24-104a24.29 24.29 0 0 0-14.01-27.6z"></path></svg>
+                    <a href="tel:+375255259000">+375255259000</a>
+                </span>
+            </div>
+        </div>
+    </div>
+</header>
+<section>
+    <div class="fire-bg">
+        <h2 class="wow rubberBand2"><span>ДАЕМ ОТПОР</span> ЖАРЕ!</h2>
+        <img class="fire-img" src="image/fire.jpg" alt="">
+        <h3 class="fire-bg-sub-title">Победа будет за нами</h3>
+    </div>
+</section>
+<!--Заправка кондиционера-->
+<section class="bg-slider">
+    <div class="container">
+        <div class="row wow bounceInUp">
+            <div class="col-md-12">
+                <h1 class="bg-slider-title text-center fonts_def_1 title-konder">Заправка кондиционера</h1>
+            </div>
+            <div class="col-lg-8">
+                <div class="picture_slider text-center ">
+                    <img class="picture-img" src="image/ystanovka.jpg">
+                </div>
+            </div>
+            <div class="col-sm-12 col-12 col-lg-4 bg-slider-title-block">
+                <div class="">
+                    <ul class="about_product_sub_title">
+                        <li class="fonts_def_1">Диагностика и поиск неисправности нерабочей системы</li>
+                        <li class="fonts_def_1">Ремонт и устранение причины неисправности с последующей заправкой</li>
+                        <li class="fonts_def_1">Бесплатный выезд в удобное время</li>
+                    </ul>
+                    <p class="fonts_def_1 about_product_title_3">Цена от: <span class="color_rer">27 руб!</span> (при
+                        заказе с сайта)</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--Устали от жары? Значит, мы едем к вам!!-->
+<section class="bg-slider">
+    <div class="container">
+        <div class="row wow bounceInUp">
+            <div class="col-md-12">
+                <h1 class="bg-slider-title text-center fonts_def_1 title-konder">Устали от жары? Значит, мы едем к
+                    вам!!</h1>
+            </div>
+            <div class="col-lg-8">
+                <div class="picture_slider text-center row">
+                    <div class="col-md-6 col-xs-6">
+                        <img class="picture-img pavki-1" src="image/egg.jpg">
+                    </div>
+                    <div class="col-md-6 col-xs-6">
+                        <img class="picture-img pavki-1" src="image/girl.jpg">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--Победа будет за нами-->
+<section class="bg-slider">
+    <div class="container">
+        <div class="row wow bounceInUp">
+            <div class="col-md-12">
+                <h1 class="bg-slider-title text-center fonts_def_1 title-konder">Победа будет за нами</h1>
+            </div>
+            <div class="col-lg-8">
+                <div class="picture_slider text-center row">
+                    <div class="col-md-6 col-xs-6">
+                        <img class="picture-img pavki-2" src="image/gory.jpg">
+                    </div>
+                    <div class="col-md-6 col-xs-6">
+                        <img class="picture-img pavki-2" src="image/wheater.jpg">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--Какие услуги мы предоставляем?-->
+<section class="">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h3 class="box6-title fonts_def_1">
+                    <span>Какие услуги мы предоставляем?</span>
+                </h3>
+            </div>
+            <div class="col-md-4 wow bounceInLeft">
+                <h4 class="text-center box_title fonts_def_1">Заправка кондиционера<br></h4>
+                <p class="text-center box_descr">Мы приехжаем к вам или вы приезжаете к нам.</p>
+                <div class="text-center">
+                    <img class="img_width_240px" src="image/zapravka.png">
+                </div>
+                <div class="price_order text-center color_rer fonts_def_1">
+                    27 руб
+                </div>
+                <div class="text-center mr_bottom">
+                    <a href="tel:+375255259000">
+                        <input type="button" class="btn fonts_def_1 btn-primarys btn_csu" value="Позвонить">
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-4 wow bounceInUp">
+                <h4 class="text-center box_title fonts_def_1">Поиск неисправности<br></h4>
+                <p class="text-center box_descr">Только если это не связано с электрикой</p>
+                <div class="text-center">
+                    <img class="img_width_240px" src="image/search.jpg">
+                </div>
+                <div class="price_order text-center color_rer fonts_def_1">
+                    10 руб
+                </div>
+                <div class="text-center mr_bottom">
+                    <a href="tel:+375255259000">
+                        <input type="button" class="btn fonts_def_1 btn-primarys btn_csu" value="Позвонить">
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-4 wow bounceInRight">
+                <h4 class="text-center box_title fonts_def_1">Поиск неисправности<br></h4>
+                <p class="text-center box_descr">Только если это не связано с электрикой</p>
+                <div class="text-center">
+                    <img class="img_width_240px" src="image/search.jpg">
+                </div>
+                <div class="price_order text-center color_rer fonts_def_1">
+                    10 руб
+                </div>
+                <div class="text-center mr_bottom">
+                    <a href="tel:+375255259000">
+                        <input type="button" class="btn fonts_def_1 btn-primarys btn_csu" value="Позвонить">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!--КАК МЫ РАБОТАЕМ?-->
+<section class="">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h3 class="box6-title">
+                    <span>КАК МЫ РАБОТАЕМ?</span>
+                </h3>
+            </div>
+            <div data-wow-delay="0.1s" class="col-md-4 wow bounceInLeft">
+                <h4 class="text-center fonts_def_1 title_workP">1 этап</h4>
+                <p class="text-center fonts_def_1 descr_workp">Ваша заявка, запись</p>
+            </div>
+            <div data-wow-delay="0.1s" class="col-md-4 wow bounceInUp">
+                <h4 class="text-center fonts_def_1 title_workP">2 этап</h4>
+                <p class="text-center fonts_def_1 descr_workp">Проверка работоспособности</p>
+            </div>
+            <div data-wow-delay="0.1s" class="col-md-4 wow bounceInRight">
+                <h4 class="text-center fonts_def_1 title_workP">3 этап</h4>
+                <p class="text-center fonts_def_1 descr_workp">Заправка, в случае работоспособности системы</p>
+            </div>
+            `
+        </div>
+    </div>
+</section>
+
+<!--КЛИЕНТЫ О НАС-->
+<section class="">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h3 class="box6-title">
+                    <span>КЛИЕНТЫ О НАС</span>
+                </h3>
+            </div>
+            <div data-wow-delay="0.2s" class="col-md-4 wow bounceInRight about_auto">
+                <div class="container-car-img">
+                    <img class="image-feedback" src="image/car3.webp" alt="авто">
+                </div>
+                <div class="box8-author fonts_def_1 text-center">Александр Вагулис</div>
+                <div class="box8-work text-center">Renault laguna 2</div>
+                <p class="box_li fonts_def_1 text-coment">Приехал на заправку кондиционера на Renault Laguna 2.
+                    Но в процессе заправки обнаружилась проблема, на месте нашли проблему( был забит осушитель)на
+                    следующий день поменяли осушитель и клапан распылитель. Заправили кондиционер (все холодит) .
+                    Сделали скидку на заправку.
+                    Плюсом является низкая стоимость, качество, клиентооринтированность и советы по эксплуатации
+                    кондиционера.
+                </p>
+            </div>
+
+            <div data-wow-delay="0.2s" class="col-md-4 wow bounceInUp about_auto">
+                <div class="container-car-img">
+                    <img class="image-feedback" src="image/car3.webp" alt="авто">
+                </div>
+                <div class="box8-author fonts_def_1 text-center">Александр Вагулис</div>
+                <div class="box8-work text-center">Renault laguna 2</div>
+                <p class="box_li fonts_def_1">Приехал на заправку кондиционера на Renault Laguna 2.
+                    Но в процессе заправки обнаружилась проблема, на месте нашли проблему( был забит осушитель)на
+                    следующий день поменяли осушитель и клапан распылитель. Заправили кондиционер (все холодит) .
+                    Сделали скидку на заправку.
+                    Плюсом является низкая стоимость, качество, клиентооринтированность и советы по эксплуатации
+                    кондиционера.
+                </p>
+            </div>
+
+            <div data-wow-delay="0.2s" class="col-md-4 wow bounceInLeft about_auto">
+                <div class="container-car-img">
+                    <img class="image-feedback" src="image/car3.webp" alt="авто">
+                </div>
+                <div class="box8-author fonts_def_1 text-center">Александр Вагулис</div>
+                <div class="box8-work text-center">Renault laguna 2</div>
+                <p class="box_li fonts_def_1">Приехал на заправку кондиционера на Renault Laguna 2.
+                    Но в процессе заправки обнаружилась проблема, на месте нашли проблему( был забит осушитель)на
+                    следующий день поменяли осушитель и клапан распылитель. Заправили кондиционер (все холодит) .
+                    Сделали скидку на заправку.
+                    Плюсом является низкая стоимость, качество, клиентооринтированность и советы по эксплуатации
+                    кондиционера.
+                </p>
+            </div>
+        </div>
+</section>
+
+<!--Как к нам доехать-->
+<section class="bg-plasti_window">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h3 class="box6-title">
+                    <span>Как к нам доехать</span>
+                </h3>
+            </div>
+        </div>
+        <div data-wow-delay="0.1s" class="text-center wow rubberBand">
+            <div id="map" class="map"></div>
+        </div>
+    </div>
+</section>
+
+<!--footer-->
+<footer class="bg-footer">
+    <div class='container'>
+        <div class='row footer-text'>
+            <div class="col-md-6 footer-right">
+                <ul class="footer-list">
+                    <li><strong>Телефон: </strong><a class="phone-link" href="tel:+375255259000">+375 25 525 9000</a>
+                    </li>
+                    <li><strong>E-mail: </strong> <a href="mailto:bymaximus17@gmail.com" class="mail-link"
+                                                     title="e-mail">example17@gmail.com</a></li>
+                    <li>"ИП Бондарчик В.И."</li>
+                </ul>
+            </div>
+            <div class="col-md-6 footer-left">
+                <ul>
+                    <li>График работы:</li>
+                    <li>пн-пт: 10.00-22.00</li>
+                    <li>сб: 11.00-21.00</li>
+                    <li>вс: выходной</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
+<script type="text/javascript">
+    // Как только будет загружен API и готов DOM, выполняем инициализацию
+    ymaps.ready(init);
+
+    function init() {
+        // Создание экземпляра карты и его привязка к контейнеру с
+        // заданным id ("map")
+        var myMap = new ymaps.Map('map', {
+            // При инициализации карты, обязательно нужно указать
+            // ее центр и коэффициент масштабирования
+            center: [53.839902, 27.544869], // Нижний Новгород
+            zoom: 17
+        });
+
+        // Создание метки
+        var myGeoObject = new ymaps.GeoObject({
+            // Геометрия.
+            geometry: {
+                // Тип геометрии - точка
+                type: "Point",
+                // Координаты точки.
+                coordinates: [53.839902, 27.544869]
+            }
+        });
+
+        // Добавление метки на карту
+        myMap.geoObjects.add(myGeoObject);
+
+
+    }
+</script>
+<script src="//code-ya.jivosite.com/widget/SMk03YbNoX" async></script>
+<script src="libs/wow_js/index.min.js"></script>
+<script>
+    wow = new WOW(
+        {
+            mobile: false,
+        }
+    )
+    wow.init();
+</script>
+</body>
+</html>
